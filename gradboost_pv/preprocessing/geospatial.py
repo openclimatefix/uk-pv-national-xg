5c726b815452eb3698d1b334f3aa1817486a30ac
Tom Armstrong <thomasarmstrong98@gmail.com>
2023-01-13 11:56:37 +0000

    refactoring repo

--- gradboost_pv/preprocessing/geospatial.py
+++ gradboost_pv/preprocessing/geospatial.py
@@ -1,4 +1,4 @@
-from typing import Union, Iterable
+from typing import Tuple, Union, Iterable
 from shapely.geometry import MultiPolygon, Polygon, Point
 from shapely.ops import unary_union
 import requests
@@ -6,6 +6,8 @@ import geopandas as gpd
 from ocf_datapipes.utils.geospatial import osgb_to_lat_lon
 import numpy as np
 import multiprocessing as mp
+import xarray as xr
+import itertools
 
 ESO_GEO_JSON_URL = (
     "https://data.nationalgrideso.com/backend/dataset/2810092e-d4b2-472f-b955-d8bea01f9ec0/"
@@ -33,6 +35,24 @@ def get_eso_uk_multipolygon() -> MultiPolygon:
         )
 
 
+def generate_polygon_mask(
+    coordinates_x: Iterable[int], coordinates_y: Iterable[int], polygon: MultiPolygon
+) -> np.ndarray:
+    coords = list(
+        map(
+            lambda x: Point(x[0], x[1]), itertools.product(coordinates_x, coordinates_y)
+        )
+    )
+
+    # create a mask for belonging to UK region or not
+    mask = check_points_in_multipolygon_multiprocessed(coords, polygon)
+    mask = mask.reshape(len(coordinates_x), len(coordinates_y)).T
+    mask = mask.astype(float)
+    mask[mask == 0] = np.nan
+
+    return mask
+
+
 def check_point_in_multipolygon(
     point: Point, polygon: Union[MultiPolygon, Polygon]
 ) -> bool:
@@ -50,3 +70,51 @@ def check_points_in_multipolygon_multiprocessed(
         for result in pool.starmap(check_point_in_multipolygon, items):
             results.append(result)
     return np.asarray(results)
+
+
+class NWPUKRegionMaskedDatasetBuilder:
+    def __init__(
+        self, nwp: xr.Dataset, evaluation_timepoints: Iterable[np.datetime64]
+    ) -> None:
+        self.nwp = nwp
+        self.eval_timepoints = evaluation_timepoints
+        self.mask = self.load_mask()
+
+    def load_mask(self) -> xr.DataArray:
+        uk_polygon = get_eso_uk_multipolygon()
+        mask = generate_polygon_mask(
+            self.nwp.coords["x"], self.nwp.coords["y"], uk_polygon
+        )
+
+        # convert numpy array to xarray mask for a 1 variable, 1 step times series of (x,y) coords
+        mask = xr.DataArray(
+            np.tile(mask.T, (len(self.nwp.coords["init_time"]), 1, 1)),
+            dims=["init_time", "x", "y"],
+        )
+        return mask
+
+    def build_region_masked_covariates(
+        self,
+        variable: str,
+        step: int,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        uk_region = xr.where(
+            ~self.mask.isnull(), self.nwp.isel(step=step).sel(variable=variable), np.nan
+        ).mean(dim=["x", "y"])
+        outer_region = xr.where(
+            self.mask.isnull(), self.nwp.isel(step=step).sel(variable=variable), np.nan
+        ).mean(dim=["x", "y"])
+
+        # interpolate both to the common GSP time points
+        uk_region = (
+            uk_region.interp(init_time=self.eval_timepoints, method="linear")
+            .to_array()
+            .as_numpy()
+        )
+        outer_region = (
+            outer_region.interp(init_time=self.eval_timepoints, method="linear")
+            .to_array()
+            .as_numpy()
+        )
+
+        return (uk_region, outer_region)
--- scripts/preprocessing/pretrained.py
+++ scripts/preprocessing/pretrained.py
@@ -3,44 +3,22 @@ import xarray as xr
 from torchdata.datapipes import functional_datapipe
 from torchdata.datapipes.iter import IterDataPipe
 import torch
-import pickle
 from torchvision.models import resnet101
 from ocf_datapipes.load.nwp.nwp import OpenNWPIterDataPipe
+import pandas as pd
 
-from typing import Callable, Iterable, Optional, Union
-from scipy.ndimage import zoom
+from typing import Callable, Iterable, Optional, Tuple, Union, Iterator
 import datetime as dt
+from math import ceil, floor
 
 
 PRETRAINED_OUTPUT_DIMS = 1_000
 NWP_VARIABLE_NUM = 17
+NWP_STEP_HORIZON = 37
 NWP_FPATH = (
     "gs://solar-pv-nowcasting-data/NWP/UK_Met_Office/UKV_intermediate_version_3.zarr/"
 )
 GSP_FPATH = "gs://solar-pv-nowcasting-data/PV/GSP/v5/pv_gsp.zarr"
-NUM_OBS = 35_000
-
-
-def _downsample_and_process_for_pretrained_input(
-    nwp_data_time_slice: xr.DataArray,
-) -> torch.Tensor:
-    nwp_data_time_slice = nwp_data_time_slice.as_numpy().values
-    nwp_data_time_slice = np.nan_to_num(nwp_data_time_slice)
-    nwp_data_time_slice = np.tile(
-        zoom(nwp_data_time_slice, (1, 64 / 704, 64 / 548), order=1), (3, 1, 1, 1)
-    ).reshape(17, 3, 64, 64)
-    return torch.from_numpy(nwp_data_time_slice)
-
-
-def _downsample_pretrained_output(model_output: torch.Tensor) -> np.ndarray:
-    with torch.no_grad():
-        output = torch.softmax(model_output, 1)
-        output = np.split(output, range(200, PRETRAINED_OUTPUT_DIMS, 200), axis=1)
-        dsampled_output = torch.concat(
-            [torch.linalg.norm(x, axis=1).reshape(1, NWP_VARIABLE_NUM) for x in output]
-        ).T
-        assert dsampled_output.shape == (NWP_VARIABLE_NUM, 5)
-        return dsampled_output.numpy().flatten()
 
 
 @functional_datapipe("process_nwp_pretrained")
@@ -48,10 +26,13 @@ class ProcessNWPPretrainedIterDataPipe(IterDataPipe):
     def __init__(
         self,
         base_nwp_datapipe: IterDataPipe,
-        step: int,
         pretrained_model: Callable[[torch.Tensor], torch.Tensor],
+        step: int,
+        batch_size: int = 50,
         interpolate: bool = False,
-        interpolation_timepoints=Optional[Iterable[Union[dt.datetime, np.datetime64]]],
+        interpolation_timepoints: Optional[
+            Iterable[Union[dt.datetime, np.datetime64]]
+        ] = None,
     ) -> None:
         if interpolate:
             assert (
@@ -60,50 +41,124 @@ class ProcessNWPPretrainedIterDataPipe(IterDataPipe):
         self.source_datapipe = base_nwp_datapipe
         self.step = step
         self.pretrained_model = pretrained_model
+        self.batch_size = batch_size
+        self.interpolate = interpolate
         self.interpolation_timepoints = interpolation_timepoints
 
-    def __iter__(self):
+    def process_for_pretrained_input(self, nwp_batch: xr.DataArray) -> torch.Tensor:
+        nwp_batch = nwp_batch.to_numpy()
+        n = nwp_batch.shape[0]  # batch size for all but the last batch
+        nwp_batch = nwp_batch.reshape(NWP_VARIABLE_NUM * n, 64, 68)
+        nwp_batch = np.tile(nwp_batch, (3, 1, 1, 1)).reshape(
+            NWP_VARIABLE_NUM * n, 3, 64, 68
+        )
+        nwp_batch = torch.from_numpy(np.nan_to_num(nwp_batch))
+        return nwp_batch
+
+    def downsample_pretrained_output(self, model_output: torch.Tensor) -> np.ndarray:
+        with torch.no_grad():
+            output = torch.softmax(model_output, 1)
+            output = np.split(output, range(200, PRETRAINED_OUTPUT_DIMS, 200), axis=1)
+            n = torch.linalg.norm(output[0], axis=1).shape[0]
+            dsampled_output = torch.concat(
+                [torch.linalg.norm(x, axis=1).reshape(1, n) for x in output]
+            ).T
+        return dsampled_output.numpy()
+
+    def __iter__(self) -> Iterator[Tuple[pd.DatetimeIndex, np.ndarray]]:
         for nwp in self.source_datapipe:
             nwp = nwp.isel(step=self.step)  # select the horizon we want
-            nwp.interp(
-                init_time_utc=self.interpolation_timepoints, method="linear"
-            )  # interpolate to perscribed points
-            for time, nwp_by_init_time in nwp.groupby("init_time_utc"):
-                # at each time point, pass the nwp data to pretrained model
-                data = _downsample_and_process_for_pretrained_input(nwp_by_init_time)
-                data = self.pretrained_model(data)
-                data = _downsample_pretrained_output(data)
-                yield (time, data)
+            if self.interpolate:
+                nwp = nwp.interp(
+                    init_time_utc=self.interpolation_timepoints, method="linear"
+                )
+            for batch_idx in range(
+                1, ceil(len(nwp.init_time_utc) / self.batch_size) + 1
+            ):
+                batch = nwp.isel(
+                    init_time_utc=slice(
+                        self.batch_size * (batch_idx - 1), self.batch_size * batch_idx
+                    )
+                )
+                batch = batch.coarsen(
+                    dim={
+                        "x_osgb": floor(548 / 64),
+                        "y_osgb": floor(704 / 64),
+                    },
+                    boundary="trim",
+                ).mean()
+                time_slice = batch.coords["init_time_utc"].values
+                with torch.no_grad():
+                    batch = self.process_for_pretrained_input(batch)
+                    output = self.pretrained_model(batch)
+                    output = self.downsample_pretrained_output(output)
+                yield (time_slice, output)
 
 
 if __name__ == "__main__":
-    gsp_path = "gs://solar-pv-nowcasting-data/PV/GSP/v5/pv_gsp.zarr"
-    gsp = xr.open_zarr(gsp_path)
-    interp_timepoints = gsp["datetime_gmt"].values[
-        (gsp["datetime_gmt"] >= np.datetime64("2020-01-01"))
-        & (gsp["datetime_gmt"] < np.datetime64("2022-01-01"))
-    ]
+    gsp = xr.open_zarr(GSP_FPATH)
+    nwp = xr.open_zarr(NWP_FPATH)
+
+    BATCH_SIZE = 1_000
+
+    # get a common 30 minute interval timeseries for GSP
+    evaluation_timeseries = (
+        gsp.coords["datetime_gmt"]
+        .where(
+            (gsp["datetime_gmt"] >= nwp.coords["init_time"].values[0])
+            & (gsp["datetime_gmt"] <= nwp.coords["init_time"].values[-1]),
+            drop=True,
+        )
+        .values
+    )
+
+    # init base NWP datapipe
     base_nwp_dpipe = OpenNWPIterDataPipe(NWP_FPATH)
     model = resnet101(pretrained=True)
-    for step in range(1):
+
+    # for each forecast horizon, proprocess and save the data locally.
+    for step in range(NWP_STEP_HORIZON):
         process_nwp_dpipe = ProcessNWPPretrainedIterDataPipe(
             base_nwp_dpipe,
-            step,
             model,
-            interpolate=True,
-            interpolation_timepoints=interp_timepoints,
+            step,
+            batch_size=BATCH_SIZE,
+            interpolate=False,  # cheaper to interpolate afterwards
         )
+
         results = []
         count = 0
         for tstamp, data in process_nwp_dpipe:
             results.append((tstamp, data))
             count += 1
-            if count % 1_000:
-                print(f"Completed obs: {count}")
-            if count >= NUM_OBS:
+            if count >= ceil(len(nwp.init_time) / BATCH_SIZE):
+                # only run the preprocessing through one cycle of data
                 break
 
-        with open(
-            f"/home/tom/local_data/pretrained_nwp_processing_step_{step}.pkl", "wb"
-        ) as f:
-            pickle.dump(results, f)
+        # format the results into a format to save and use later
+
+        # generate column names of our data
+        columns = list()
+        for variable in nwp.coords["variable"].values:
+            for idx in range(5):
+                columns.append(f"{variable}_{idx}")
+
+        # put it all into a DataFrame to save locally and load later.
+        # interpolate our pretrained features to 1/2 hourly interval, same
+        # as out GSP target.
+        results = pd.concat(
+            [
+                pd.DataFrame(
+                    res[1].reshape(len(res[0]), 17 * 5), columns=columns, index=res[0]
+                )
+                .reindex(pd.date_range(start=res[0][0], end=res[0][-1], freq="0.5H"))
+                .interpolate(method="linear", axis=0)
+                for res in results
+            ]
+        ).sort_index()
+
+        results.to_pickle(
+            f"/home/tom/local_data/pretrained_nwp_processing_step_{step}.pkl"
+        )
+
+        print(f"Complete Pretrained Proprocessing for step: {step}")
