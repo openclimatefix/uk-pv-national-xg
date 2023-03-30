62dd8b9a38000700fe61770c6591b9c044931f21
peterdudfield <peter.dudfield@hotmail.com>
2023-03-30 18:39:46 +0100

    add smoother and test

--- gradboost_pv/inference/utils.py
+++ gradboost_pv/inference/utils.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 from typing import List
 
 import pandas as pd
+import numpy as np
 import pvlib
 from nowcasting_datamodel.models import ForecastSQL
 
@@ -82,3 +83,27 @@ def calculate_azimuth_and_elevation_angle(
 
     # extract the information we want
     return solpos[["elevation", "azimuth"]]
+
+
+def smooth_results(results_df: pd.Series):
+    """
+    Smooth results by taking the averaging across 3 of data hours
+
+    We take use the weights [25%, 50%, 25%] to smooth the data.
+    At the edges we use the last values to fill in the missing data.
+    At the edges we could use the edge and next edge,
+    but this might lead to some biased results, like pulling the result up if in the morning.
+
+    """
+
+    # use rolling average
+    weights = np.array([0.25, 0.5, 0.25])
+    smoothed_df = results_df.rolling(window=3, center=True).apply(lambda x: np.sum(weights * x))
+
+    # adjust the first and last values
+    smoothed_df.iloc[0] = results_df.iloc[0]
+    smoothed_df.iloc[-1] = results_df.iloc[-1]
+
+    return smoothed_df
+
+
