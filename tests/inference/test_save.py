7e50b2161c6218851bf7263b0d68652add90d1bc
peterdudfield <peter.dudfield@hotmail.com>
2023-04-11 09:46:03 +0100

    save twice

--- tests/inference/test_save.py
+++ tests/inference/test_save.py
@@ -26,3 +26,24 @@ def test_save_to_database(db_session):
     assert len(db_session.query(ForecastSQL).all()) == 2  # 1 normal, 1 historic
     assert len(db_session.query(ForecastValueSQL).all()) == 5
     assert len(db_session.query(ForecastValueLatestSQL).all()) == 5
+
+
+@freeze_time("2023-01-01")
+def test_save_to_database_twice(db_session):
+    results_df = pd.DataFrame(
+        columns=["datetime_of_target_utc", "forecast_kw"],
+        data=[
+            [datetime(2023, 1, 1), 7.3],
+            [datetime(2023, 1, 1, 1), 8.3],
+            [datetime(2023, 1, 1, 2), 9.3],
+        ],
+    )
+
+    save_to_database(session=db_session, results_df=results_df)
+    save_to_database(session=db_session, results_df=results_df)
+
+    db_session.flush()
+
+    assert len(db_session.query(ForecastSQL).all()) == 3  # 2 normal, 1 historic
+    assert len(db_session.query(ForecastValueSQL).all()) == 10
+    assert len(db_session.query(ForecastValueLatestSQL).all()) == 5
