f06d1aed170f746df01f7fb9854166dc9f4e6361
Jacob Bieker <jacob@bieker.tech>
2023-07-07 14:42:16 +0100

    Update train script

--- scripts/models/train/region_filtered_model.py
+++ scripts/models/train/region_filtered_model.py
@@ -78,7 +78,7 @@ def main(path_to_processed_nwp: Path, nwp_variables: list[str]) -> Dict[Hour, Ex
 
     results = dict()
 
-    for forecast_horizon_hour in [0, 1, 2, 4, 8, 12, 24, 36]:
+    for forecast_horizon_hour in range(37):
         print(forecast_horizon_hour)
         # independently fit an XGBoost model for each forecast horizon
         processed_nwp = load_all_variable_slices(
@@ -90,7 +90,7 @@ def main(path_to_processed_nwp: Path, nwp_variables: list[str]) -> Dict[Hour, Ex
         X, y = build_datasets_from_local(
             processed_nwp, gsp_data, np.timedelta64(forecast_horizon_hour, "h")
         )
-        training_results = run_experiment(X, y, DEFFAULT_HYPARAM_CONFIG)
+        training_results = run_experiment(X, y, DEFFAULT_HYPARAM_CONFIG, forecast_hour=forecast_horizon_hour)
 
         results[forecast_horizon_hour] = training_results
 
