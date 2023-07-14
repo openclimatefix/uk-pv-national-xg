aa7505548c8fe5a7569fc9a63528f431951acd73
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-07-14 10:28:21 +0100

    ocf_datapipes==1.2.41 and fic for plevels (#93)
    * ocf_datapipes==1.2.41
    
    * fix for plevel cols
    
    * update test

--- tests/inference/test_save.py
+++ tests/inference/test_save.py
@@ -16,11 +16,16 @@ from nowcasting_datamodel.models import (
 @freeze_time("2023-01-01")
 def test_save_to_database(db_session, me_latest):
     results_df = pd.DataFrame(
-        columns=["datetime_of_target_utc", "forecast_mw"],
+        columns=[
+            "datetime_of_target_utc",
+            "forecast_mw",
+            "forecast_mw_plevel_10",
+            "forecast_mw_plevel_90",
+        ],
         data=[
-            [datetime(2023, 1, 1), 7.3],
-            [datetime(2023, 1, 1, 1), 8.3],
-            [datetime(2023, 1, 1, 2), 9.3],
+            [datetime(2023, 1, 1), 7.3, 7.1, 7.5],
+            [datetime(2023, 1, 1, 1), 8.3, 8.1, 8.5],
+            [datetime(2023, 1, 1, 2), 9.3, 9.1, 9.5],
         ],
     )
 
@@ -30,5 +35,8 @@ def test_save_to_database(db_session, me_latest):
 
     assert len(db_session.query(ForecastSQL).all()) == 2  # 1 normal, 1 historic
     assert len(db_session.query(ForecastValueSQL).all()) == 5
-    assert len(db_session.query(ForecastValueLatestSQL).all()) == 5
+    latest = db_session.query(ForecastValueLatestSQL).all()
+    assert len(latest) == 5
+    assert latest[0].properties is not None
+    assert latest[1].properties is not None
     assert len(db_session.query(ForecastValueSevenDaysSQL).all()) == 5
