8234e853ef6bd0d955d163003ea63223c7397f3d
peterdudfield <peter.dudfield@hotmail.com>
2024-05-28 22:45:27 +0100

    add test

--- tests/inference/test_datafeeds.py
+++ tests/inference/test_datafeeds.py
@@ -1,3 +1,4 @@
+from ocf_datapipes.config.load import load_yaml_configuration
 from gradboost_pv.inference.data_feeds import ProductionDataFeed
 from freezegun import freeze_time
 import pandas as pd
@@ -8,6 +9,10 @@ def test_production_datafeed(sample_prod_nwp_data, sample_prod_gsp_data, model_c
     data = {"nwp": sample_prod_nwp_data, "gsp": sample_prod_gsp_data}
 
     _ = data_feed.post_process(data=data)
+
+
+def test_production_load_config():
+    _ = load_yaml_configuration(filename='./configs/default_production_datafeed.yaml')
 
 
 @freeze_time("2021-05-25 12:15:00")
