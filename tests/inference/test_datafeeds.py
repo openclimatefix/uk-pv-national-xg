79166c90f9a9546b4c8989d14cc0973ca5f5c845
peterdudfield <peter.dudfield@hotmail.com>
2024-05-29 08:27:51 +0100

    make test more resislant

--- tests/inference/test_datafeeds.py
+++ tests/inference/test_datafeeds.py
@@ -1,7 +1,14 @@
-from ocf_datapipes.config.load import load_yaml_configuration
-from gradboost_pv.inference.data_feeds import ProductionDataFeed
-from freezegun import freeze_time
+import os
+
 import pandas as pd
+from freezegun import freeze_time
+from ocf_datapipes.config.load import load_yaml_configuration
+
+import gradboost_pv
+from gradboost_pv.inference.data_feeds import ProductionDataFeed
+
+# get main dir folder
+PATH_TO_CONFIG_DIRECTORY = os.path.dirname(gradboost_pv.__file__) + "/../configs"
 
 
 def test_production_datafeed(sample_prod_nwp_data, sample_prod_gsp_data, model_config_path):
@@ -12,7 +19,9 @@ def test_production_datafeed(sample_prod_nwp_data, sample_prod_gsp_data, model_c
 
 
 def test_production_load_config():
-    _ = load_yaml_configuration(filename='./configs/default_production_datafeed.yaml')
+    _ = load_yaml_configuration(
+        filename=f"{PATH_TO_CONFIG_DIRECTORY}/default_production_datafeed.yaml"
+    )
 
 
 @freeze_time("2021-05-25 12:15:00")
