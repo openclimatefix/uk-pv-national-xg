80a22cc59568445fc7ac39f0780ee8bda65a2174
peterdudfield <peter.dudfield@hotmail.com>
2025-02-27 17:20:55 +0000

    rename UKV variables

--- gradboost_pv/inference/data_feeds.py
+++ gradboost_pv/inference/data_feeds.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Iterator, Optional, Union
+from importlib.resources import files
 
 import numpy as np
 import pandas as pd
@@ -15,6 +16,7 @@ import xarray as xr
 from ocf_datapipes.config.load import load_yaml_configuration
 from ocf_datapipes.config.model import Configuration
 from ocf_datapipes.load import OpenGSPFromDatabase
+from ocf_datapipes.utils.geospatial import lon_lat_to_osgb
 from torch.utils.data import IterDataPipe, functional_datapipe
 
 from gradboost_pv.models.utils import load_nwp_coordinates
@@ -106,6 +108,52 @@ class ProductionOpenNWPNetcdfIterDataPipe(IterDataPipe):
                 chunks=None,  # Reading Satellite Zarr benefits from setting chunks='auto'
                 # (see issue #456) but 'auto' massively slows down reading NWPs.
             )
+
+            # if um-ukv is in the datavars, then this comes from the new new-consuerm
+            # We need to rename the data variables, and
+            # load in lat and lon, ready for regridding later.
+            if 'um-ukv' in nwp.data_vars:
+
+                logger.info("Renaming the UKV variables")
+
+                # rename to UKV
+                nwp = nwp.rename({"um-ukv": "UKV"})
+
+                variable_coords = nwp.variable.values
+                rename = {"cloud_cover_high": "hcc",
+                          "cloud_cover_low": "lcc",
+                          "cloud_cover_medium": "mcc",
+                          "cloud_cover_total": "tcc",
+                          "snow_depth_gl": "sde",
+                          "direct_shortwave_radiation_flux_gl": "sr",
+                          "downward_longwave_radiation_flux_gl": "dlwrf",
+                          "downward_shortwave_radiation_flux_gl": "dswrf",
+                          "downward_ultraviolet_radiation_flux_gl": "duvrs",
+                          "relative_humidity_sl": "r",
+                          "temperature_sl": "t",
+                          "total_precipitation_rate_gl": "prate",
+                          "visibility_sl": "vis",
+                          "wind_direction_10m": "wdir10",
+                          "wind_speed_10m": "si10",
+                          "wind_v_component_10m": "v10",
+                          "wind_u_component_10m": "u10"}
+
+                for k, v in rename.items():
+                    variable_coords[variable_coords == k] = v
+
+                # assign the new variable names
+                nwp = nwp.assign_coords(variable=variable_coords)
+
+                # this is all taken from the metoffice website, apart from the x and y values
+                lat = xr.open_dataset(files("data").joinpath("nwp-consumer-mo-ukv-lat.nc"))
+                lon = xr.open_dataset(files("data").joinpath("nwp-consumer-mo-ukv-lon.nc"))
+
+                # convert lat, lon to osgb
+                x,y = lon_lat_to_osgb(lon.longitude.values, lat.latitude.values)
+
+                # combine with d, and just taking a 1-d array.
+                nwp = nwp.assign_coords(x_osgb=x[0])
+                nwp = nwp.assign_coords(y_osgb=y[:,0])
 
             if self.nwp_channels is not None:
                 logger.info(
